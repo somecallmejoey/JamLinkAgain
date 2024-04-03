d2bfe4264361738f2b31f28df2d84d982e4a6266
Joey G <121167275+somecallmejoey@users.noreply.github.com>
2024-04-03 13:24:03 +0100

    Initial commit

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,62 @@
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/");
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <Link to="/register">
+            <span>Don't have an account?</span>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            {err && <span className="error">{err}</span>}
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [err, setErr] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Update the API endpoint to match JamLink's registration endpoint
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <Link to="/login">
+            <span>Already have an account?</span>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder=" Name"
+              name="name"
+              onChange={handleChange}
+            />
+            {err && <span className="error">{err}</span>}
+            <button onClick={handleClick}>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
